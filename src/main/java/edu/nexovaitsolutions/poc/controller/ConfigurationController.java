6725f0d498f8d824f6c13b603a7c2360b0b3ac67
nimantha1999 <nimantha.bt@gmail.com>
2025-09-19 21:04:51 +0530

    changed DB dto and updated the controller to validations

--- src/main/java/edu/nexovaitsolutions/poc/controller/ConfigurationController.java
+++ src/main/java/edu/nexovaitsolutions/poc/controller/ConfigurationController.java
@@ -4,6 +4,7 @@ package edu.nexovaitsolutions.poc.controller;
 import edu.nexovaitsolutions.poc.dto.DB;
 import edu.nexovaitsolutions.poc.service.ConfigurationService;
 import edu.nexovaitsolutions.poc.service.impl.ConfigurationServiceImpl;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +25,7 @@ public class ConfigurationController {
     }
 
     @PostMapping("db/add")
-    public ResponseEntity<?> addDB(@RequestBody DB db){
+    public ResponseEntity<?> addDB(@Valid @RequestBody DB db){
         int save = configurationService.save(db);
         if (save>0){
             return ResponseEntity.ok("Saved");
--- src/main/java/edu/nexovaitsolutions/poc/dto/DB.java
+++ src/main/java/edu/nexovaitsolutions/poc/dto/DB.java
@@ -1,5 +1,6 @@
 package edu.nexovaitsolutions.poc.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 @AllArgsConstructor
@@ -9,9 +10,22 @@ import lombok.*;
 @ToString
 public class DB {
     private Integer id;
+    @NotBlank(message = "Host cannot be empty")
     private String host;
+    @NotBlank(message = "UserName cannot be empty")
     private String username;
+    @Min(value = 1024, message = "Port must be >=1024")
+    @Max(value = 65535, message = "Port must be <=65535")
+
     private Integer port;
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,20}$",
+            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character"
+    )
+
     private String password;
+    @NotBlank(message = "DatabaseName cannot be empty")
     private String databaseName;
 }
