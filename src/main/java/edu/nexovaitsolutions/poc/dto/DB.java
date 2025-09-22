aa92b7e7ef926f6fde9b86c7cf4413799da62e63
nimantha1999 <nimantha.bt@gmail.com>
2025-09-22 13:18:06 +0530

    validated host, username,db name

--- src/main/java/edu/nexovaitsolutions/poc/dto/DB.java
+++ src/main/java/edu/nexovaitsolutions/poc/dto/DB.java
@@ -2,6 +2,7 @@ package edu.nexovaitsolutions.poc.dto;
 
 import jakarta.validation.constraints.*;
 import lombok.*;
+import org.hibernate.validator.constraints.URL;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -11,8 +12,10 @@ import lombok.*;
 public class DB {
     private Integer id;
     @NotBlank(message = "Host cannot be empty")
+    @URL(message = "Host name is invalid.")
     private String host;
     @NotBlank(message = "UserName cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z][A-Za-z0-9_]*$", message = "Username name is invalid. Name should be start with a letter and must contain a letter, number or _")
     private String username;
     @Min(value = 1024, message = "Port must be >=1024")
     @Max(value = 65535, message = "Port must be <=65535")
@@ -21,11 +24,13 @@ public class DB {
     @NotBlank(message = "Password cannot be empty")
     @Size(min = 8, message = "Password must be at least 8 characters long")
     @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,20}$",
-            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character"
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!()-?><'\"/|;:`~]).{8,20}$",
+            message = "Password must contain at least one digit, one lowercase, one uppercase, and one special character (@#$%^&+=!()-?><'\"/|;:`~)"
     )
 
     private String password;
     @NotBlank(message = "DatabaseName cannot be empty")
+    @Pattern(regexp = "^[a-zA-Z][A-Za-z0-9_]*$", message = "Database name is invalid. Name should be start with a letter and must contain a letter, number or _")
+
     private String databaseName;
 }
